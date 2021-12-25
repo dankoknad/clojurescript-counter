ce3425b6e96f1e6977aa4fca80d360fe16b9b5e0
Danko Milutinovic <dankomilutinovic@gmail.com>
2021-12-25 23:55:51 +0100

    creating reset-btn component

--- src/reagent01/components.cljs
+++ src/reagent01/components.cljs
@@ -2,8 +2,23 @@
   (:require 
      [clojure.string :as s]
      [reagent-mui.components :as mui]
+     [reagent-mui.icons.close :refer [close]]
      [reagent-mui.material.button :refer [button]]
   ))
+
+(defn reset-btn [handle-click disabled]
+  [mui/icon-button
+    {:size "small"
+     :on-click handle-click
+     :disabled disabled
+     :style {:position "absolute"
+             :top 26
+             :right -40
+             :color "#fff"
+             :background (if disabled "#e2e2e2" "#d32f2f")}
+    }
+    [close]
+  ])
 
 (defn btn [label handle-click disabled color]
   [button
@@ -27,5 +42,5 @@
      :on-change handle-change
      :style {:vertical-align "middle"}
     }
-  ]
-)
+  ])
+
--- src/reagent01/core.cljs
+++ src/reagent01/core.cljs
@@ -4,7 +4,6 @@
       [reagent.dom :as d]
       [reagent01.state :as s]
       [reagent-mui.components :as mui]
-      [reagent-mui.icons.close :refer [close]]
       [reagent01.components :as c]
       [reagent01.misc :as m]
       ))
@@ -17,17 +16,10 @@
     [mui/grid {:item true} "Simple Counter"]
     [mui/grid {:item true :style {:position "relative"}}
       [mui/paper m/paper-props [@s/state :count]]
-      [mui/icon-button {
-        :size "small"
-        :on-click #(swap! s/state assoc-in [:count] (@s/state :initial))
-        :disabled (if (== (@s/state :count) (@s/state :initial)) true false)
-        :style {:position "absolute"
-                :top 26
-                :right -40
-                :color "#fff"
-                :background (if (== (@s/state :count) (@s/state :initial)) "#e2e2e2" "#d32f2f")}
-        } [close]]
-      ]
+      [c/reset-btn
+        #(swap! s/state assoc-in [:count] (@s/state :initial))
+        (== (@s/state :count) (@s/state :initial))
+      ]]
 
     [mui/grid {:item true} 
       [:<> "Step: "]
