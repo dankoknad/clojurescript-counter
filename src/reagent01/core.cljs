c52806b042d5713feedd81aa763f111ec97a2eb8
Danko Milutinovic <dankomilutinovic@gmail.com>
2021-12-25 23:27:14 +0100

    adding mui-icons, disabling reset button

--- src/reagent01/core.cljs
+++ src/reagent01/core.cljs
@@ -4,6 +4,7 @@
       [reagent.dom :as d]
       [reagent01.state :as s]
       [reagent-mui.components :as mui]
+      [reagent-mui.icons.close :refer [close]]
       [reagent01.components :as c]
       [reagent01.misc :as m]
       ))
@@ -16,17 +17,16 @@
     [mui/grid {:item true} "Simple Counter"]
     [mui/grid {:item true :style {:position "relative"}}
       [mui/paper m/paper-props [@s/state :count]]
-      [mui/fab {
-        :children "x"
+      [mui/icon-button {
         :size "small"
+        :on-click #(swap! s/state assoc-in [:count] (@s/state :initial))
+        :disabled (if (== (@s/state :count) (@s/state :initial)) true false)
         :style {:position "absolute"
-                :top 24
+                :top 26
                 :right -40
                 :color "#fff"
-                :background "#d32f2f"
-                :display (if (== (@s/state :count) (@s/state :initial)) "none" "block")}
-        :on-click #(swap! s/state assoc-in [:count] (@s/state :initial))
-        }]
+                :background (if (== (@s/state :count) (@s/state :initial)) "#e2e2e2" "#d32f2f")}
+        } [close]]
       ]
 
     [mui/grid {:item true} 
@@ -35,8 +35,7 @@
         (@s/state :step)
         #(swap! s/state assoc-in [:step] (-> % .-target .-value int))
         (@s/state :input-disabled)
-      ]
-      ]
+      ]]
 
     [mui/grid {:item true} 
       [c/btn
@@ -46,10 +45,9 @@
       [c/btn
         "Increment"
         #(swap! s/state assoc-in [:count] (+ (@s/state :count) (@s/state :step)))
-        (@s/state :increment-btn-disabled)]
-      ]
-    ]
-  )
+        (@s/state :increment-btn-disabled)]]
+    
+  ])
 
 ;; -------------------------
 ;; Initialize app
